b093220ac935a757666bb25c481335233f190af7
Dennis <suwatchai.kaew@gmail.com>
2023-12-25 22:02:48 +0700

    feat: add finished ex1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,63 @@
 import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState();
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  async function getData() {
+    try {
+      const result = await axios.get("http://localhost:4001/products");
+      setData(result.data.data);
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+  async function handleDelete(id) {
+    await axios.delete(`http://localhost:4001/products/${id}`);
+    await getData();
+  }
   return (
     <div className="App">
       <div className="app-wrapper">
         <h1 className="app-title">Products</h1>
       </div>
       <div className="product-list">
-        <div className="product">
-          <div className="product-preview">
-            <img
-              src="https://via.placeholder.com/350/350"
-              alt="some product"
-              width="350"
-              height="350"
-            />
-          </div>
-          <div className="product-detail">
-            <h1>Product name: ...</h1>
-            <h2>Product price: ... Baht</h2>
-            <p>Product description: .....</p>
-          </div>
+        {!data ? (
+          <p>{error}</p>
+        ) : (
+          data.map(({ id, name, price, image, description }, index) => {
+            return (
+              <div key={index} className="product">
+                <div className="product-preview">
+                  <img
+                    src={image}
+                    alt="some product"
+                    width="350"
+                    height="350"
+                  />
+                </div>
+                <div className="product-detail">
+                  <h1>Product name: {name}</h1>
+                  <h2>Product price: {price} Baht</h2>
+                  <p>Product description: {description}</p>
+                </div>
 
-          <button className="delete-button">x</button>
-        </div>
+                <button
+                  className="delete-button"
+                  onClick={() => handleDelete(id)}
+                >
+                  x
+                </button>
+              </div>
+            );
+          })
+        )}
       </div>
     </div>
   );
