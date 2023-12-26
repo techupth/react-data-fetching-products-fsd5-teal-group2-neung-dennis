acaeafd428326c897bbb1a3f330bd3a72ec5ef7e
Neung0511 <neung@gmail.com>
2023-12-26 20:58:28 +0700

    feat:add ex3

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,20 +7,43 @@ function App() {
 
   const [products, setProducts] = useState([]);
 
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
   const deleteProduct = (id) => {
-    axios.delete(`${baseURL}/${id}`).then(() => {
-      setProducts((prevProducts) =>
-        prevProducts.filter((product) => product.id !== id)
-      );
-    });
+    axios
+      .delete(`${baseURL}/${id}`)
+      .then(() => {
+        setProducts((prevProducts) =>
+          prevProducts.filter((product) => product.id !== id)
+        );
+      })
+      .catch((error) => {
+        console.error("Error deleting product: ", error);
+      });
   };
 
   useEffect(() => {
-    axios.get(baseURL).then((response) => {
-      setProducts(response.data.data);
-      // console.log(response.data.data);
-    });
+    axios
+      .get(baseURL)
+      .then((response) => {
+        setProducts(response.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching products: ", error);
+        setError(true);
+        setLoading(false);
+      });
   }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error occurred while fetching data</div>;
+  }
 
   return (
     <div className="App">
@@ -28,30 +51,26 @@ function App() {
         <h1 className="app-title">Products</h1>
       </div>
 
-      {products.map((products) => (
-        <div className="product-list">
+      {products.map((product) => (
+        <div className="product-list" key={product.id}>
           <div className="product">
             <div className="product-preview">
               <img
-                src={products.image}
+                src={product.image}
                 alt="some product"
                 width="350"
                 height="350"
-                key={products.id}
               />
             </div>
             <div className="product-detail">
-              <h1>{products.name}</h1>
-              <h2>{products.price}</h2>
-              <p>{products.description}</p>
+              <h1>{product.name}</h1>
+              <h2>{product.price}</h2>
+              <p>{product.description}</p>
             </div>
 
             <button
               className="delete-button"
-              onClick={
-                () => deleteProduct(products.id)
-                // console.log(products.id)
-              }>
+              onClick={() => deleteProduct(product.id)}>
               x
             </button>
           </div>
