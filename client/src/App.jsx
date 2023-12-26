1aa3301ccc9707b500ee5a5deaa420bf1f4dd65a
Neung0511 <neung@gmail.com>
2023-12-26 15:27:00 +0700

    ex1:completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,62 @@
 import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const baseURL = "http://localhost:4001/products";
+
+  const [products, setProducts] = useState([]);
+
+  const deleteProduct = (id) => {
+    axios.delete(`${baseURL}/${id}`).then(() => {
+      setProducts((prevProducts) =>
+        prevProducts.filter((product) => product.id !== id)
+      );
+    });
+  };
+
+  useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setProducts(response.data.data);
+      // console.log(response.data.data);
+    });
+  }, []);
+
   return (
     <div className="App">
       <div className="app-wrapper">
         <h1 className="app-title">Products</h1>
       </div>
-      <div className="product-list">
-        <div className="product">
-          <div className="product-preview">
-            <img
-              src="https://via.placeholder.com/350/350"
-              alt="some product"
-              width="350"
-              height="350"
-            />
-          </div>
-          <div className="product-detail">
-            <h1>Product name: ...</h1>
-            <h2>Product price: ... Baht</h2>
-            <p>Product description: .....</p>
-          </div>
 
-          <button className="delete-button">x</button>
+      {products.map((products) => (
+        <div className="product-list">
+          <div className="product">
+            <div className="product-preview">
+              <img
+                src={products.image}
+                alt="some product"
+                width="350"
+                height="350"
+                key={products.id}
+              />
+            </div>
+            <div className="product-detail">
+              <h1>{products.name}</h1>
+              <h2>{products.price}</h2>
+              <p>{products.description}</p>
+            </div>
+
+            <button
+              className="delete-button"
+              onClick={
+                () => deleteProduct(products.id)
+                // console.log(products.id)
+              }>
+              x
+            </button>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
